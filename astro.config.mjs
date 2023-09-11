bc2d0e66570c28d80ad9ff4886654afea138483d
Takagi Tasuku <tasuren@outlook.jp>
2023-09-11 17:58:54 +0900

    Starlightに移行

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "RT 2023 Documents",
+      social: {
+        discord: "https://discord.gg/aP7SG5XaTK",
+        github: 'https://github.com/aoharu-io/rt-2023-docs'
+      },
+      sidebar: [
+        {label: "一般", items: [
+          {label: "イントロダクション", link: "/docs/intro"},
+          {label: "ユーザーガイド", link: "/docs/user_guide"},
+          {label: "トラブルシューティング", link: "/docs/trouble_shooting"}
+        ]},
+        {label: "リファレンス", autogenerate: {directory: "docs/reference"}}
+      ]
+    })
+  ]
+});
---
+++ src/content/docs/index.md
@@ -0,0 +1,8 @@
+---
+title: Redirecting...
+head:
+  - tag: script
+    content: "window.location.replace('/docs/intro');"
+# なぜわざわざ`docs`ディレクトリを`docs`の中に作るかは、ドキュザウルスとの互換性を維持するためです。
+# この問題は、リニューアル後に解消される予定です。
+---
